62f9d39e59b9b5283a5f82c65fb4eb0ac05ed00d
Denisbeder <denisbeder@gmail.com>
2025-01-07 17:48:55 -0400

    add address

--- src/views/Customer/CustomerFormView.vue
+++ src/views/Customer/CustomerFormView.vue
@@ -438,7 +438,149 @@
                         <div>
                             <h2 class="card-title">Endereços</h2>
                         </div>
-                        <div class="card card--grid">END</div>
+                        <div class="card flex flex-col gap-8">
+                            <div
+                                v-for="(address, index) in form.addresses"
+                                :key="`contact_${index}`"
+                                class="relative grid grid-col-1 sm:grid-cols-6 gap-3 border bg-gray-50 p-4 rounded-md"
+                            >
+                                <div class="sm:col-span-1">
+                                    <label
+                                        for="address-zipcode"
+                                        class="label"
+                                    >
+                                        CEP
+                                    </label>
+                                    <input
+                                        id="address-zipcode"
+                                        v-model="address.zipcode"
+                                        type="text"
+                                        autocomplete="off"
+                                        class="form-input mt-2"
+                                        :name="`address_zipcode[${index}]`"
+                                    />
+                                </div>
+
+                                <div class="sm:col-span-4">
+                                    <label
+                                        for="address-street"
+                                        class="label"
+                                    >
+                                        Rua
+                                    </label>
+                                    <input
+                                        id="address-street"
+                                        v-model="address.street"
+                                        type="text"
+                                        autocomplete="off"
+                                        class="form-input mt-2"
+                                        :name="`address_street[${index}]`"
+                                    />
+                                </div>
+
+                                <div class="sm:col-span-1">
+                                    <label
+                                        for="address-number"
+                                        class="label"
+                                    >
+                                        Número
+                                    </label>
+                                    <input
+                                        id="address-number"
+                                        v-model="address.number"
+                                        type="text"
+                                        autocomplete="off"
+                                        class="form-input mt-2"
+                                        :name="`address_number[${index}]`"
+                                    />
+                                </div>
+
+                                <div class="sm:col-span-2">
+                                    <label
+                                        for="address-neighborhood"
+                                        class="label"
+                                    >
+                                        Bairro
+                                    </label>
+                                    <input
+                                        id="address-neighborhood"
+                                        v-model="address.neighborhood"
+                                        type="text"
+                                        autocomplete="off"
+                                        class="form-input mt-2"
+                                        :name="`address_neighborhood[${index}]`"
+                                    />
+                                </div>
+
+                                <div class="sm:col-span-2">
+                                    <label
+                                        for="address-city"
+                                        class="label"
+                                    >
+                                        Cidade
+                                    </label>
+                                    <input
+                                        id="address-city"
+                                        v-model="address.city"
+                                        type="text"
+                                        autocomplete="off"
+                                        class="form-input mt-2"
+                                        :name="`address_city[${index}]`"
+                                    />
+                                </div>
+
+                                <div class="sm:col-span-1">
+                                    <label
+                                        for="address-state"
+                                        class="label"
+                                    >
+                                        Estado
+                                    </label>
+                                    <input
+                                        id="address-state"
+                                        v-model="address.state"
+                                        type="text"
+                                        autocomplete="off"
+                                        class="form-input mt-2"
+                                        :name="`address_state[${index}]`"
+                                    />
+                                </div>
+
+                                <div class="sm:col-span-1">
+                                    <label
+                                        for="address-complement"
+                                        class="label"
+                                    >
+                                        Complemento
+                                    </label>
+                                    <input
+                                        id="address-complement"
+                                        v-model="address.complement"
+                                        type="text"
+                                        autocomplete="off"
+                                        class="form-input mt-2"
+                                        :name="`address_complement[${index}]`"
+                                    />
+                                </div>
+
+                                <button
+                                    type="button"
+                                    title="Remover contato"
+                                    class="absolute top-0 right-0 translate-x-1/2 -translate-y-1/2 btn btn--square btn--white btn--sm"
+                                    @click="handleRemoveAddress(index)"
+                                >
+                                    <XIcon class="size-8" />
+                                </button>
+                            </div>
+
+                            <button
+                                type="button"
+                                class="btn btn--primary"
+                                @click="handleAddAddress"
+                            >
+                                <PlusIcon class="size-4" /> Add Endereço
+                            </button>
+                        </div>
                     </div>
                 </div>
             </LoadingComponent>
