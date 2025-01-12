a2f58cf7721728ee05fde150a9b61c13708d59bf
Denisbeder <denisbeder@gmail.com>
2025-01-12 15:41:10 -0400

    customize page 404

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,4 +1,13 @@
 <template>
-    <h1>404</h1>
-    <h3>Não encontrado!</h3>
+  <main class="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+    <div class="text-center">
+      <p class="text-2xl font-semibold text-primary">404</p>
+      <h1 class="mt-4 text-balance text-4xl font-semibold tracking-tight text-gray-900 sm:text-7xl">Página não encontrada</h1>
+      <p class="mt-6 text-pretty text-lg font-medium text-gray-500 sm:text-xl/8">Desculpe, mas não conseguimos encontrar esta página.</p>
+      <div class="mt-10 flex items-center justify-center gap-x-6">
+        <button @click="$router.back()" class="btn btn--primary">Voltar</button>
+        <!--<a href="#" class="btn btn&#45;&#45;link">Falar com o suporte <span aria-hidden="true">&rarr;</span></a>-->
+      </div>
+    </div>
+  </main>
 </template>
